ecd29c87173dfebbf197e8ea9c3698fa31b9c7b1
liyang <liyang9096@gmail.com>
2022-06-23 09:48:55 +0700

    代码更新

--- base_dir.py
+++ base_dir.py
@@ -17,6 +17,8 @@ base_conf_dir =os.path.join(base_dir,'bfty_config','base_conf.ini')
 #用例excel 地址
 excel_dir = r'D:\project\BfLibrary'
 owner_backer_path  = os.path.join(excel_dir,'test_data','接口测试用例.xlsx')
+agent_management_path  = os.path.join(excel_dir,'test_data','代理管理测试用例.xlsx')
+main_station_report_path  = os.path.join(excel_dir,'test_data','报表管理测试用例.xlsx')
 token_url = os.path.join(excel_dir,'test_data','token.yaml')
 config_url = os.path.join(excel_dir,'config','config.yaml')
 
