1608f0da5fb3ba005350eec7e522ac841b979220
liyang <liyang9096@gmail.com>
2022-07-13 18:35:12 +0700

    代理报表-总代未完成交易-新增测试用例失败重跑/代码优化

--- test_case_excel/main_station_agentReport/test_unsettled_order.py
+++ test_case_excel/main_station_agentReport/test_unsettled_order.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2022/6/20 10:03
 # @Author  : liyang
-# @FileName: test_unsettled_order.py
+# @FileName: 总台-代理报表-总代未完成交易
 # @Software: PyCharm
-
 
 import pytest
 import allure,os
 import sys
-import datetime
-import requests
 sys.path.append(os.getcwd())
 
 from mde_CreditBackground import CreditBackGround
@@ -19,17 +16,34 @@ from common.do_excel import DoExcel
 from CommonFunc import CommonFunc
 from base_dir import *
 from tools.yamlControl import Yaml_data
-from config import cfile
 
+# 获取环境配置
+configure = Yaml_data().get_yaml_data(fileDir=config_url, isAll=True)
+mysql_info = []
+mongo_info = []
+if configure[0]['environment'] == "mde":
+    mysql_dic = configure[1]['mysql_mde']
+    mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
+    mongo_dic = configure[1]['mongodb_mde']
+    mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
+elif configure[0]['environment'] == "120":
+    mysql_dic = configure[1]['mysql_config']
+    mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
+    mongo_dic = configure[1]['mongodb_config']
+    mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
+else:
+    raise AssertionError('ERROR,this environment is not available')
+
+# 测试用例失败重跑,作用于类下面的所有用例
+@pytest.mark.flaky(reruns=3, reruns_delay=10)
 @allure.feature('总台-未完成交易')
 class Test_unsettledOrder:
 
     # 读取excle 里面的用例
     de = DoExcel(file_name=owner_backer_path, sheet_name="unsettledOrder")
     case_list1 = de.get_case(de.get_sheet())
-
     @pytest.mark.parametrize('excel_data', case_list1)
-    @pytest.mark.skip(reason='调试代码,暂不执行')
+    # @pytest.mark.skip(reason='调试代码,暂不执行')
     @allure.story('总台-代理报表-未完成交易')
     def test_unsettledOrder(self, excel_data):
         '''
@@ -38,22 +52,6 @@ class Test_unsettledOrder:
         :param sport_params: excel中的参数化数据
         :return:
         '''
-        configure = Yaml_data().get_yaml_data(fileDir=config_url, isAll=True)
-        mysql_info = []
-        mongo_info = []
-        if configure[0]['environment'] == "http://35.234.4.41:31101/mock/message":
-            mysql_dic = configure[1]['mysql_mde']
-            mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
-            mongo_dic = configure[1]['mongodb_mde']
-            mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
-        elif configure[0]['environment'] == "http://192.168.10.10:8808/mock/message":
-            mysql_dic = configure[1]['mysql_config']
-            mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
-            mongo_dic = configure[1]['mongodb_config']
-            mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
-        else:
-            raise AssertionError('ERROR,this environment is not available')
-
         if excel_data[12] == '是':
             # 读取参数化数据
             request_body = eval(excel_data[6])             # 将字符串转成相应的对象（如list、tuple、dict和string之间的转换）
@@ -62,30 +60,24 @@ class Test_unsettledOrder:
             with allure.step(f"执行测试用例:{title}"):
                 Bf_log('unsettledOrder').info(f"----------------开始执行:{title}------------------------")
 
-            # 获取接口地址
+            # 获取接口地址和请求方法
             request_url = CreditBackGround(mysql_info, mongo_info).mde_url + excel_data[4]
             with allure.step(f"请求地址： {request_url}"):
                 Bf_log('unsettledOrder').info(f'请求地址为：{request_url}')
+            request_method = excel_data[5]
 
-            # token = Yaml_data().get_yaml_data(fileDir=token_url, isAll=True)[0]['token']
-            # token = cfile.read_yaml(yaml_file=token_url)[0]['token']
-            token = CreditBackGround(mysql_info, mongo_info).login_background(uname='Liyang01', password='Bfty123456',securityCode='111111', loginDiv=222333)
+            get_token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method=request_method,request_url=request_url,
+                                                                                request_body=request_body)
             head = {"LoginDiv": "222333",
                     "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Account_Login_Identify": token,
+                    "Account_Login_Identify": get_token,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
             # 执行接口的请求
-            request_method = excel_data[5]
             response_data = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()
 
             actualResult = []
             if response_data['message'] == 'OK':
-                APIResult_list = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()['data']
-                for item in APIResult_list:
-                    actualResult.append([item['account'],item['name'],item['levelName'],item['currency'],item['numberOfBets'],item['totalBet'],
-                                             item['level0Percentage'],item['companyPercentage']])
-            elif response_data['code'] != '50025':
                 APIResult_list = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()['data']
                 for item in APIResult_list:
                     actualResult.append([item['account'],item['name'],item['levelName'],item['currency'],item['numberOfBets'],item['totalBet'],
@@ -121,32 +113,33 @@ class Test_unsettledOrder:
                                     else:
                                         api_result = float(aip_data)
                                     new_item1.append(api_result)
-                                new_item1.insert(0, item1[0])
-                                new_item1.insert(1, item1[1])
-                                new_item1.insert(2, item1[2])
-                                new_item1.insert(3, item2[3])
+                                index = 0
+                                for item in item1[:4]:
+                                    new_item1.insert(index, item)
+                                    index += 1
                                 for sql_data in item2[4:]:
                                     if sql_data == None or sql_data == 0:
                                         sql_result = 0
                                     else:
                                         sql_result = float(sql_data)
                                     new_item2.append(sql_result)
-                                new_item2.insert(0, item2[0])
-                                new_item2.insert(1, item2[1])
-                                new_item2.insert(2, item2[2])
-                                new_item2.insert(3, item2[3])
+                                index = 0
+                                for item in item2[:4]:
+                                    new_item2.insert(index, item)
+                                    index += 1
+
                                 # 判断两个list的值是否一致,并且回写入excel
                                 if new_item1 == new_item2:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
                                         Bf_log('unsettledOrder').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
                                         DoExcel(owner_backer_path, "unsettledOrder").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试通过 \n{ctime}")
                                 else:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
                                         Bf_log('unsettledOrder').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
                                         DoExcel(owner_backer_path, "unsettledOrder").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试不通过 \n{ctime}")
                                 assert new_item1 == new_item2
 
@@ -175,22 +168,6 @@ class Test_unsettledOrder:
         :param sport_params: excel中的参数化数据
         :return:
         '''
-        configure = Yaml_data().get_yaml_data(fileDir=config_url, isAll=True)
-        mysql_info = []
-        mongo_info = []
-        if configure[0]['environment'] == "http://35.234.4.41:31101/mock/message":
-            mysql_dic = configure[1]['mysql_mde']
-            mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
-            mongo_dic = configure[1]['mongodb_mde']
-            mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
-        elif configure[0]['environment'] == "http://192.168.10.10:8808/mock/message":
-            mysql_dic = configure[1]['mysql_config']
-            mysql_info.extend([mysql_dic['host'], mysql_dic['user'], mysql_dic['password'], mysql_dic['port']])
-            mongo_dic = configure[1]['mongodb_config']
-            mongo_info.extend([mongo_dic['user'], mongo_dic['password'], mongo_dic['host'], mongo_dic['port']])
-        else:
-            raise AssertionError('ERROR,this environment is not available')
-
         if excel_data[12] == '是':
             # 读取参数化数据
             params_list = sport_params
@@ -202,21 +179,20 @@ class Test_unsettledOrder:
             with allure.step(f"执行测试用例:{title}"):
                 Bf_log('unsettledOrder').info(f"----------------开始执行:{title}------------------------")
 
-            # 获取接口地址
+            # 获取接口地址和请求方法
             request_url = CreditBackGround(mysql_info, mongo_info).mde_url + excel_data[4]
             with allure.step(f"请求地址： {request_url}"):
                 Bf_log('unsettledOrder').info(f'请求地址为：{request_url}')
+            request_method = excel_data[5]
 
-            # token = Yaml_data().get_yaml_data(fileDir=token_url, isAll=True)[0]['token']
-            # token = cfile.read_yaml(yaml_file=token_url)[0]['token']
-            token = CreditBackGround(mysql_info, mongo_info).login_background(uname='Liyang01', password='Bfty123456',securityCode='111111', loginDiv=222333)
+            get_token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method=request_method,request_url=request_url,
+                                                                                request_body=request_body)
             head = {"LoginDiv": "222333",
                     "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Account_Login_Identify": token,
+                    "Account_Login_Identify": get_token,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
             # 执行接口的请求
-            request_method = excel_data[5]
             response_data = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()
             bet_dic = {1:'单关', 2:'串关', 3:'复式串关'}
             odds_dic = {"1": '欧洲盘', "2": '香港盘'}
@@ -234,7 +210,7 @@ class Test_unsettledOrder:
                         actualResult.append([item['account'],item['memberName'],item['orderNo'],create_time,item['sportsType'],bet_type,
                                              [detail['tournamentName'],detail['homeTeamName'] + ' Vs ' + detail['awayTeamName'],detail['matchType'], detail['marketName'],
                                               detail['specifier'], detail['outcomeName'], detail['odds'], odds_type,detail['matchTimeStr']],
-                                             item['betAmount'],item['betResult'],item['betIp'] + ' / ' + item['betIpAddress'],item['betAmount'],item['companyPercentage'],
+                                             item['betResult'],item['betIp'] + ' / ' + item['betIpAddress'],item['betAmount'],item['companyPercentage'],
                                              item['level0Percentage'],item['level0CommissionRatio'],item['level1Percentage'],item['level1CommissionRatio'],item['level2Percentage'],
                                              item['level2CommissionRatio'],item['level3Percentage'],item['level3CommissionRatio'],item['memberCommissionRatio']])
 
@@ -264,8 +240,9 @@ class Test_unsettledOrder:
                     matchTime = item[14]
                     match_time = matchTime.strftime("%Y-%m-%d %H:%M:%S")
                     expectResult.append([item[0],item[1],item[2],create_time,item[4],item[5],
-                                         [item[6], item[7], item[8], item[9], item[10], item[11], item[12], item[13],match_time],
-                                         item[14],item[15],item[16],item[17],item[18],item[19],item[20],item[21],item[22],item[23],item[24],item[25],item[26],item[27]])
+                                         [item[6], item[7], item[8], item[9], item[10], item[11], float(item[12]), item[13],match_time],
+                                         item[16],item[17],float(item[15]), float(item[18]),item[19],item[20],item[21],item[22],item[23],
+                                         item[24],item[25],item[26],item[27]])
 
                 expect_result = CommonFunc().merge_compelx_02(new_lList=expectResult)
 
@@ -275,7 +252,7 @@ class Test_unsettledOrder:
                 if actual_result != [] or expect_result != []:
                     for index1, item1 in enumerate(actual_result):
                         for index2, item2 in enumerate(expect_result):
-                            if item1[2] == item2[2]:     # 判断账号/登入账号是否相等,若相等,则校验该条数据
+                            if item1[2] == item2[2]:     # 判断注单号是否相等,若相等,则校验该条数据
                                 new_item1 = []
                                 new_item2 = []
                                 for aip_data in item1[11:]:
@@ -284,44 +261,33 @@ class Test_unsettledOrder:
                                     else:
                                         api_result = float(aip_data)
                                     new_item1.append(api_result)
-                                new_item1.insert(0, item1[0])
-                                new_item1.insert(1, item1[1])
-                                new_item1.insert(2, item1[2])
-                                new_item1.insert(3, item1[3])
-                                new_item1.insert(4, item1[4])
-                                new_item1.insert(5, item1[5])
-                                new_item1.insert(6, item1[6])
-                                new_item1.insert(7, item1[7])
-                                new_item1.insert(8, item1[8])
-                                new_item1.insert(9, item1[9])
+                                index = 0
+                                for item in item1[:11]:
+                                    new_item1.insert(index, item)
+                                    index += 1
                                 for sql_data in item2[11:]:
                                     if sql_data == None or sql_data == 0:
                                         sql_result = 0
                                     else:
                                         sql_result = float(sql_data)
                                     new_item2.append(sql_result)
-                                new_item2.insert(0, item2[0])
-                                new_item2.insert(1, item2[1])
-                                new_item2.insert(2, item2[2])
-                                new_item2.insert(3, item2[3])
-                                new_item2.insert(4, item1[4])
-                                new_item2.insert(5, item1[5])
-                                new_item2.insert(6, item1[6])
-                                new_item2.insert(7, item1[7])
-                                new_item2.insert(8, item1[8])
-                                new_item2.insert(9, item1[9])
+                                index = 0
+                                for item in item2[:11]:
+                                    new_item2.insert(index, item)
+                                    index += 1
+
                                 # 判断两个list的值是否一致,并且回写入excel
                                 if new_item1 == new_item2:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
                                         Bf_log('unsettledOrder').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
                                         DoExcel(owner_backer_path, "unsettledOrder").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试通过 \n{ctime}")
                                 else:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
                                         Bf_log('unsettledOrder').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
                                         DoExcel(owner_backer_path, "unsettledOrder").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试不通过 \n{ctime}")
                                 assert new_item1 == new_item2
 
@@ -336,5 +302,5 @@ class Test_unsettledOrder:
 
 if __name__ == "__main__":
 
-    pytest.main(["test_unsettled_order.py",'-vs', '-q', '--alluredir', '../report/tmp', '--clean-alluredir'])
+    pytest.main(["test_unsettled_order.py",'-vs', '-q', '--alluredir', '../report/tmp'])  # '--clean-alluredir'
     os.system("allure serve ../report/tmp")
