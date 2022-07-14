f780aad515535d51e7f5c68483a6a6d3b97d9c65
liyang <liyang9096@gmail.com>
2022-07-14 12:17:45 +0700

    代理报表-总代盈亏(简易)-新增测试用例失败重跑/代码优化

--- test_case_excel/main_station_agentReport/test_ProfitAndLossSimple.py
+++ test_case_excel/main_station_agentReport/test_ProfitAndLossSimple.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2022/6/21 19:22
 # @Author  : liyang
-# @FileName: test_ProfitAndLossSimple.py
+# @FileName: 总台-代理报表-总代盈亏(简易)
 # @Software: PyCharm
-
 
 import pytest
 from pytest_assume.plugin import assume
-# from pytest import assume
 import allure,os
 import sys
-import datetime
-import requests
 sys.path.append(os.getcwd())
 
 from mde_CreditBackground import CreditBackGround
@@ -21,8 +17,6 @@ from common.do_excel import DoExcel
 from CommonFunc import CommonFunc
 from base_dir import *
 from tools.yamlControl import Yaml_data
-from config import cfile
-
 
 # 获取环境配置
 configure = Yaml_data().get_yaml_data(fileDir=config_url, isAll=True)
@@ -41,7 +35,8 @@ elif configure[0]['environment'] == "120":
 else:
     raise AssertionError('ERROR,this environment is not available')
 
-
+# 测试用例失败重跑,作用于类下面的所有用例
+@pytest.mark.flaky(reruns=3, reruns_delay=10)
 @allure.feature('总台-总代盈亏(简易)')
 class Test_profitAndLossSimple:
 
@@ -49,7 +44,7 @@ class Test_profitAndLossSimple:
     de = DoExcel(file_name=owner_backer_path, sheet_name="eventEntered")
     case_list1 = de.get_case(de.get_sheet())
     @pytest.mark.parametrize('excel_data', case_list1)
-    # @pytest.mark.skip(reason='调试代码,暂不执行')
+    @pytest.mark.skip(reason='调试代码,暂不执行')
     @allure.story('总台-代理报表-总代盈亏(简易)-输入赛事')
     def test_eventEntered(self, excel_data):
         '''
@@ -65,21 +60,20 @@ class Test_profitAndLossSimple:
             with allure.step(f"执行测试用例:{title}"):
                 Bf_log('eventEntered').info(f"----------------开始执行:{title}------------------------")
 
-            # 获取接口地址
+            # 获取接口地址和请求方法
             request_url = CreditBackGround(mysql_info, mongo_info).mde_url + excel_data[4]
             with allure.step(f"请求地址： {request_url}"):
                 Bf_log('eventEntered').info(f'请求地址为：{request_url}')
+            request_method = excel_data[5]
 
-            token = Yaml_data().get_yaml_data(fileDir=token_url, isAll=True)[0]['token']
-            # token = cfile.read_yaml(yaml_file=token_url)[0]['token']
-            # token = CreditBackGround(mysql_info, mongo_info).login_background(uname='Liyang01', password='Bfty123456',securityCode='111111', loginDiv=222333)
+            get_token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method=request_method,request_url=request_url,
+                                                                                request_body=request_body)
             head = {"LoginDiv": "222333",
                     "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Account_Login_Identify": token,
+                    "Account_Login_Identify": get_token,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
             # 执行接口的请求
-            request_method = excel_data[5]
             response_data = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()
 
             actualResult = []
@@ -164,7 +158,7 @@ class Test_profitAndLossSimple:
     de = DoExcel(file_name=owner_backer_path, sheet_name="winLoseSimple")
     case_list1 = de.get_case(de.get_sheet())
     @pytest.mark.parametrize('excel_data', case_list1)
-    @pytest.mark.skip(reason='调试代码,暂不执行')
+    # @pytest.mark.skip(reason='调试代码,暂不执行')
     @allure.story('总台-代理报表-总代盈亏(简易)')
     def test_profitAndLossSimple(self, excel_data):
         '''
@@ -182,26 +176,24 @@ class Test_profitAndLossSimple:
             with allure.step(f"执行测试用例:{title}"):
                 Bf_log('ProfitAndLossSimple').info(f"----------------开始执行:{title}------------------------")
 
-            # 获取接口地址
+            # 获取接口地址和请求方法
             request_url = CreditBackGround(mysql_info, mongo_info).mde_url + excel_data[4]
             with allure.step(f"请求地址： {request_url}"):
                 Bf_log('ProfitAndLossSimple').info(f'请求地址为：{request_url}')
+            request_method = excel_data[5]
 
-            # token = Yaml_data().get_yaml_data(fileDir=token_url, isAll=True)[0]['token']
-            # token = cfile.read_yaml(yaml_file=token_url)[0]['token']
-            token = CreditBackGround(mysql_info, mongo_info).login_background(uname='Liyang01', password='Bfty123456',securityCode='111111', loginDiv=222333)
+            get_token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method=request_method,request_url=request_url,
+                                                                                request_body=request_body)
             head = {"LoginDiv": "222333",
                     "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Account_Login_Identify": token,
+                    "Account_Login_Identify": get_token,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
             # 执行接口的请求
-            request_method = excel_data[5]
             response_data = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()
             actualResult = []
             if response_data['message'] == 'OK':
                 APIResult_list = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()['data']
-                # print(APIResult_list)
                 for item in APIResult_list:
                     if item['levelName'] == '登0':
                         actualResult.append([item['account'],item['name'],item['levelName'],item['currency'],item['numberOfBets'],item['totalBet'],item['totalEfficientAmount'],
@@ -223,7 +215,7 @@ class Test_profitAndLossSimple:
                 actualResult = []
             else:
                 raise AssertionError(response_data['message'])
-            # print(actualResult)
+
             # 执行SQL,SQL写法f{"参数"}
             sql_str = eval(excel_data[7])
             SQLResult_list = list(MysqlFunc(mysql_info, mongo_info).query_data(sql_str, db_name='bfty_credit'))
@@ -250,32 +242,33 @@ class Test_profitAndLossSimple:
                                     else:
                                         api_result = float(aip_data)
                                     new_item1.append(api_result)
-                                new_item1.insert(0, item1[0])
-                                new_item1.insert(1, item1[1])
-                                new_item1.insert(2, item1[2])
-                                new_item1.insert(3, item1[3])
+                                index = 0
+                                for item in item1[:4]:
+                                    new_item1.insert(index, item)
+                                    index += 1
                                 for sql_data in item2[4:]:
                                     if sql_data == None or sql_data == 0:
                                         sql_result = 0
                                     else:
                                         sql_result = float(sql_data)
                                     new_item2.append(sql_result)
-                                new_item2.insert(0, item2[0])
-                                new_item2.insert(1, item2[1])
-                                new_item2.insert(2, item2[2])
-                                new_item2.insert(3, item1[3])
+                                index = 0
+                                for item in item2[:4]:
+                                    new_item2.insert(index, item)
+                                    index += 1
+
                                 # 判断两个list的值是否一致,并且回写入excel
                                 if new_item1 == new_item2:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
                                         Bf_log('ProfitAndLossSimple').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
                                         DoExcel(owner_backer_path, "winLoseSimple").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试通过 \n{ctime}")
                                 else:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
                                         Bf_log('ProfitAndLossSimple').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
                                         DoExcel(owner_backer_path, "winLoseSimple").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试不通过 \n{ctime}")
                                 assert new_item1 == new_item2
 
@@ -296,7 +289,7 @@ class Test_profitAndLossSimple:
     case_list2 = de.get_case(de.get_sheet())
     @pytest.mark.parametrize('excel_data', case_list1)
     @pytest.mark.parametrize('sport_params', case_list2)
-    @pytest.mark.skip(reason='调试代码,暂不执行')
+    # @pytest.mark.skip(reason='调试代码,暂不执行')
     @allure.story('总台-代理报表-总代盈亏(简易)-注单详情')
     def test_profitAndLossOrderSimple(self, excel_data, sport_params):
         '''
@@ -318,21 +311,20 @@ class Test_profitAndLossSimple:
             with allure.step(f"执行测试用例:{title}"):
                 Bf_log('profitAndLossOrderSimple').info(f"----------------开始执行:{title}------------------------")
 
-            # 获取接口地址
+            # 获取接口地址和请求方法
             request_url = CreditBackGround(mysql_info, mongo_info).mde_url + excel_data[4]
             with allure.step(f"请求地址： {request_url}"):
                 Bf_log('profitAndLossOrderSimple').info(f'请求地址为：{request_url}')
+            request_method = excel_data[5]
 
-            # token = Yaml_data().get_yaml_data(fileDir=token_url, isAll=True)[0]['token']
-            # token = cfile.read_yaml(yaml_file=token_url)[0]['token']
-            token = CreditBackGround(mysql_info, mongo_info).login_background(uname='Liyang01', password='Bfty123456',securityCode='111111', loginDiv=222333)
+            get_token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method=request_method,request_url=request_url,
+                                                                                request_body=request_body)
             head = {"LoginDiv": "222333",
                     "Accept-Language": "zh-CN,zh;q=0.9",
-                    "Account_Login_Identify": token,
+                    "Account_Login_Identify": get_token,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
             # 执行接口的请求
-            request_method = excel_data[5]
             response_data = CreditBackGround(mysql_info, mongo_info).bf_request(method=request_method, url=request_url, head=head,data=request_body).json()
             bet_dic = {"1":'单关', "2":'串关', "3":'复式串关'}
             odds_dic = {"1": '欧洲盘', "2": '香港盘'}
@@ -384,7 +376,7 @@ class Test_profitAndLossSimple:
                     matchTime = item[14]
                     match_ime = matchTime.strftime("%Y-%m-%d %H:%M:%S")
                     expectResult.append([item[0],item[1],item[2],create_time,item[4],item[5],
-                                         [item[6],item[7],item[8],item[9],item[10],item[11],item[12],item[13],match_ime],
+                                         [item[6],item[7],item[8],item[9],item[10],item[11],float(item[12]),item[13],match_ime],
                                          item[15],item[16],item[17],item[18],item[19],item[20],item[21],item[22],item[23],item[24],item[25],item[26],item[27],
                                          item[28],item[29],item[30],item[31],item[32],item[33],item[34],item[35],item[36],item[37],item[38],item[39],item[40],
                                          item[41],item[42], item[43],item[44], item[45],item[46], item[47],item[48], item[49]])
@@ -406,49 +398,36 @@ class Test_profitAndLossSimple:
                                     else:
                                         api_result = float(aip_data)
                                     new_item1.append(api_result)
-                                new_item1.insert(0, item1[0])
-                                new_item1.insert(1, item1[1])
-                                new_item1.insert(2, item1[2])
-                                new_item1.insert(3, item1[3])
-                                new_item1.insert(4, item1[4])
-                                new_item1.insert(5, item1[5])
-                                new_item1.insert(6, item1[6])
-                                new_item1.insert(7, item1[7])
-                                new_item1.insert(8, item1[8])
-                                new_item1.insert(9, item1[9])
+                                index = 0
+                                for item in item1[:10]:
+                                    new_item1.insert(index, item)
+                                    index += 1
                                 for sql_data in item2[10:]:
                                     if sql_data == None or sql_data == 0:
                                         sql_result = 0
                                     else:
                                         sql_result = float(sql_data)
                                     new_item2.append(sql_result)
-                                new_item2.insert(0, item2[0])
-                                new_item2.insert(1, item2[1])
-                                new_item2.insert(2, item2[2])
-                                new_item2.insert(3, item2[3])
-                                new_item2.insert(4, item1[4])
-                                new_item2.insert(5, item1[5])
-                                new_item2.insert(6, item1[6])
-                                new_item2.insert(7, item1[7])
-                                new_item2.insert(8, item1[8])
-                                new_item2.insert(9, item1[9])
+                                index = 0
+                                for item in item2[:10]:
+                                    new_item2.insert(index, item)
+                                    index += 1
 
                                 # 判断两个list的值是否一致,并且回写入excel
                                 if new_item1 == new_item2:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
                                         Bf_log('profitAndLossOrderSimple').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
                                         DoExcel(owner_backer_path, "winLoseOrderSimple").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试通过 \n{ctime}")
                                 else:
                                     with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
                                         Bf_log('profitAndLossOrderSimple').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
                                         DoExcel(owner_backer_path, "winLoseOrderSimple").write_result(row=int(excel_data[0]+1),
-                                                                                               actual_result=f'{actualResult}',expect_result=f'{expectResult}',
+                                                                                               actual_result=f'{new_item1}',expect_result=f'{new_item2}',
                                                                                                is_pass=f"测试不通过 \n{ctime}")
-                                        continue
-
-                                with assume: assert new_item1 == new_item2
+                                assert new_item1 == new_item2
+                                # with assume: assert new_item1 == new_item2
                                     # pytest.assume(new_item1 == new_item2)
 
                 else:
@@ -463,5 +442,5 @@ class Test_profitAndLossSimple:
 
 if __name__ == "__main__":
 
-    pytest.main(["test_ProfitAndLossSimple.py",'-vs', '-q', '--alluredir', '../report/tmp', '--clean-alluredir'])
+    pytest.main(["test_ProfitAndLossSimple.py",'-vs', '-q', '--alluredir', '../report/tmp']) # '--clean-alluredir'
     os.system("allure serve ../report/tmp")
