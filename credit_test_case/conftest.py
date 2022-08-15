57b961c0db53e0c439926e61ba1725eaa0d29fa8
liyang <liyang9096@gmail.com>
2022-08-15 09:58:53 +0700

    初始化配置文件新增环境名称

--- credit_test_case/conftest.py
+++ credit_test_case/conftest.py
@@ -5,11 +5,16 @@
 # @Software: PyCharm
 
 import pytest
+from tools.yamlControl import Yaml_data
+from base_dir import *
+
+configure = Yaml_data().get_yaml_data(fileDir=config_url, isAll=True)
+environment = configure[0]['environment']
 
 # 自动化测试止执行前 -- 环境初始化操作
 @pytest.fixture(scope="session",autouse=True)        #function（测试函数级别），class（测试类级别），module（测试模块“.py”级别），session（多个文件级别）。默认是function级别
 def start_running():
-    print('---马上开始执行自动化测试---')
+    print(f'--- 马上开始执行自动化测试, 当前环境为：{environment} ---')
     yield   # 通过yield来唤醒teardown执行
     print('\n---自动化测试完成,开始处理本次测试数据---')
 
