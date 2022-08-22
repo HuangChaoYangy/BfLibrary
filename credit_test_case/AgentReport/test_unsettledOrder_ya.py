b180093182db816b2527e61adb6ce5a75da35d49
liyang <liyang9096@gmail.com>
2022-08-22 19:14:12 +0700

    代理报表-总代未完成交易/-新增获取基础url配置文件

--- credit_test_case/AgentReport/test_unsettledOrder_ya.py
+++ credit_test_case/AgentReport/test_unsettledOrder_ya.py
@@ -21,83 +21,87 @@ dataBase_configure = CommonFunc().get_dataBase_environment_config()
 mysql_info = dataBase_configure[0]
 mongo_info = dataBase_configure[1]
 
+# 获取基础路径配置
+url_configure = CommonFunc().get_BaseUrl_environment_config()    # 获取配置文件中后台的ip
+ip_address = url_configure[1]
+
 # 测试用例失败重跑,作用于类下面的所有用例
-# @pytest.mark.flaky(reruns=3, reruns_delay=10)
+@pytest.mark.flaky(reruns=3, reruns_delay=10)
 @allure.feature('总台-报表管理-总代未完成交易')
 class Test_unsettledOrder_yaml(object):
 
-    # YamlFileData().get_testcase_params(csv_path=csv_url_unsettle, yaml_file=unsettle_url, new_yaml_file=unsettle_url_new)
-    # yaml_data = Yaml_data().read_yaml_file(yaml_file=unsettle_url_new, isAll=False)
-    # url_data = Yaml_data().read_yaml_file(yaml_file=unsettle_url_new, isAll=True)[0]['request']
+    YamlFileData().get_testcase_params(csv_path=csv_url_unsettle, yaml_file=unsettle_url, new_yaml_file=unsettle_url_new)
+    yaml_data = Yaml_data().read_yaml_file(yaml_file=unsettle_url_new, isAll=False)
+    url_data = Yaml_data().read_yaml_file(yaml_file=unsettle_url_new, isAll=True)[0]['request']
     # @pytest.mark.skip(reason="调试代码,暂不执行")
-    # @pytest.mark.parametrize('inBody, expData', yaml_data)
-    # @allure.story('报表管理-总代未完成交易')
-    # def test_unsettledOrder(self, inBody, expData, url=url_data):
-    #     '''
-    #     管理后台-报表管理-总代未完成交易
-    #     :param inBody:
-    #     :param expData:
-    #     :return:
-    #     '''
-    #     allure.dynamic.title(inBody['title'])
-    #     actualResult = CreditBackGround(mysql_info,mongo_info).credit_unsettledOrder(inData=inBody)
-    #
-    #     with allure.step(f"执行测试用例:{inBody['title']}"):
-    #         Bf_log('unsettledOrder').info(f"----------------开始执行:{inBody['title']}------------------------")
-    #     url = url['mde_ip'] + url['url']
-    #     with allure.step(f"请求地址 {url}"):
-    #         Bf_log('unsettledOrder').info(f'请求地址为:{url}')
-    #
-    #     sql = MysqlQuery(mysql_info, mongo_info).credit_unsettledOrder_query(expData=expData)[1]
-    #     with allure.step(f'查询SQL:{sql}'):
-    #         Bf_log('unsettledOrder').info(f'执行sql:{sql}')
-    #     expectResult = MysqlQuery(mysql_info, mongo_info).credit_unsettledOrder_query(expData=expData)[0]
-    #
-    #     # 校验接口数据和SQL数据的长度
-    #     if len(actualResult) == len(expectResult):
-    #         if actualResult != [] or expectResult != []:
-    #             for index1, item1 in enumerate(actualResult):
-    #                 for index2, item2 in enumerate(expectResult):
-    #                     if item1[0] == item2[0]:  # 判断账号/登入账号是否相等,若相等,则校验该条数据
-    #                         new_item1 = []
-    #                         new_item2 = []
-    #                         for aip_data in item1[4:]:
-    #                             if aip_data == None or aip_data == 0:
-    #                                 api_result = 0
-    #                             else:
-    #                                 api_result = float(aip_data)
-    #                             new_item1.append(api_result)
-    #                         index = 0
-    #                         for item in item1[:4]:
-    #                             new_item1.insert(index, item)
-    #                             index += 1
-    #                         for sql_data in item2[4:]:
-    #                             if sql_data == None or sql_data == 0:
-    #                                 sql_result = 0
-    #                             else:
-    #                                 sql_result = float(sql_data)
-    #                             new_item2.append(sql_result)
-    #                         index = 0
-    #                         for item in item2[:4]:
-    #                             new_item2.insert(index, item)
-    #                             index += 1
-    #
-    #                         # 判断两个list的值是否一致,并且回写入excel
-    #                         if new_item1 == new_item2:
-    #                             with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
-    #                                 Bf_log('unsettledOrder').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
-    #                         else:
-    #                             with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
-    #                                 Bf_log('unsettledOrder').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
-    #
-    #                         assert new_item1 == new_item2
-    #
-    #         else:
-    #             with allure.step(f'实际结果：{actualResult}, 期望结果：{expectResult},==》测试通过'):
-    #                 Bf_log('unsettledOrder').info(f'实际结果:{actualResult}, 期望结果：{expectResult},==》测试通过')
-    #
-    #     else:
-    #         raise AssertionError(f"接口查询的结果与数据库查询长度不一致!接口为{len(actualResult)},sql为{len(expectResult)}")
+    @pytest.mark.parametrize('inBody, expData', yaml_data)
+    @allure.story('报表管理-总代未完成交易')
+    def test_unsettledOrder(self, inBody, expData, url=url_data):
+        '''
+        管理后台-报表管理-总代未完成交易
+        :param inBody:
+        :param expData:
+        :return:
+        '''
+        allure.dynamic.title(inBody['title'])
+        actualResult = CreditBackGround(mysql_info,mongo_info).credit_unsettledOrder(inData=inBody)
+
+        with allure.step(f"执行测试用例:{inBody['title']}"):
+            Bf_log('unsettledOrder').info(f"----------------开始执行:{inBody['title']}------------------------")
+        url = ip_address + url['url']
+        with allure.step(f"请求地址 {url}"):
+            Bf_log('unsettledOrder').info(f'请求地址为:{url}')
+
+        sql = MysqlQuery(mysql_info, mongo_info).credit_unsettledOrder_query(expData=expData)[1]
+        with allure.step(f'查询SQL:{sql}'):
+            Bf_log('unsettledOrder').info(f'执行sql:{sql}')
+        expectResult = MysqlQuery(mysql_info, mongo_info).credit_unsettledOrder_query(expData=expData)[0]
+
+        # 校验接口数据和SQL数据的长度
+        if len(actualResult) == len(expectResult):
+            if actualResult != [] or expectResult != []:
+                for index1, item1 in enumerate(actualResult):
+                    for index2, item2 in enumerate(expectResult):
+                        if item1[0] == item2[0]:  # 判断账号/登入账号是否相等,若相等,则校验该条数据
+                            new_item1 = []
+                            new_item2 = []
+                            for aip_data in item1[4:]:
+                                if aip_data == None or aip_data == 0:
+                                    api_result = 0
+                                else:
+                                    api_result = float(aip_data)
+                                new_item1.append(api_result)
+                            index = 0
+                            for item in item1[:4]:
+                                new_item1.insert(index, item)
+                                index += 1
+                            for sql_data in item2[4:]:
+                                if sql_data == None or sql_data == 0:
+                                    sql_result = 0
+                                else:
+                                    sql_result = float(sql_data)
+                                new_item2.append(sql_result)
+                            index = 0
+                            for item in item2[:4]:
+                                new_item2.insert(index, item)
+                                index += 1
+
+                            # 判断两个list的值是否一致,并且回写入excel
+                            if new_item1 == new_item2:
+                                with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试通过'):
+                                    Bf_log('unsettledOrder').info(f'实际结果:{new_item1}, 期望结果：{new_item2},==》测试通过')
+                            else:
+                                with allure.step(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过'):
+                                    Bf_log('unsettledOrder').error(f'实际结果：{new_item1}, 期望结果：{new_item2},==》测试不通过')
+
+                            assert new_item1 == new_item2
+
+            else:
+                with allure.step(f'实际结果：{actualResult}, 期望结果：{expectResult},==》测试通过'):
+                    Bf_log('unsettledOrder').info(f'实际结果:{actualResult}, 期望结果：{expectResult},==》测试通过')
+
+        else:
+            raise AssertionError(f"接口查询的结果与数据库查询长度不一致!接口为{len(actualResult)},sql为{len(expectResult)}")
 
 
     # YamlFileData().get_testcase_params(csv_path=csv_url_unsettle_d, yaml_file=unsettle_url_d, new_yaml_file=unsettle_url_new_d)
@@ -182,19 +186,18 @@ class Test_unsettledOrder_yaml(object):
     @allure.story('报表管理-总代未完成交易-注单详情')
     def test_unsettledOrderDetail(self, inBody, expData, request=url_data):
         '''
-        管理后台-报表管理-总代未完成交易-注单详情
+        管理后台-报表管理-总代未完成交易-注单详情//传入登3账号,遍历所有会员
         :param inBody:
         :param expData:
         :return:
         '''
         betType_dic = {1:'单注', 2:'串关', 3:'复式串关'}
         odds_dic = {"1": '欧洲盘', "2": '香港盘'}
-        ip = request['mde_ip']
         url = request['url']
         method = request['method']
-        request_url = ip + url
+        request_url = ip_address + url
 
-        token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method='post', request_url='https://search.betf.best/winOrLost/proxy/bill',
+        token = CreditBackGround(mysql_info, mongo_info).get_user_token(request_method='post', request_url=ip_address + '/winOrLost/proxy/bill',
                                   request_body={"type": "", "begin": "2022-07-12", "end": "2022-07-12", "page": 1,"limit": 50})
         head = {"LoginDiv": "222333",
                 "Accept-Language": "zh-CN,zh;q=0.9",
@@ -211,10 +214,7 @@ class Test_unsettledOrder_yaml(object):
             for detail in item:
                 user_id_list.append(detail)
 
-        # ctime = CommonFunc().get_current_time_for_client(time_type="ctime", day_diff=int(inBody['ctime']))
-        # etime = CommonFunc().get_current_time_for_client(time_type="ctime", day_diff=int(inBody['etime']))
         for user_id in user_id_list:
-            # total_title = f"根据会员账号查询注单详情,  查询日期范围：'{ctime} -- {etime}'"
             total_title = f"根据会员账号查询总代未完成交易-注单详情,  查询日期范围：所有日期"
             sql_user = f"SELECT account FROM u_user where id ='{user_id}'"
             rtn = list(MysqlFunc(mysql_info, mongo_info).query_data(sql_user, db_name='bfty_credit'))
