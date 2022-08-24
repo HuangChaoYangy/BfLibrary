eb358ce857b45af535a979515e9b1242d0f6f05b
liyang <liyang9096@gmail.com>
2022-08-24 15:26:57 +0700

    代理报表-账目-新增获取基础url配置文件

--- credit_test_case/AgentReport/test_bill_ya.py
+++ credit_test_case/AgentReport/test_bill_ya.py
@@ -20,6 +20,10 @@ dataBase_configure = CommonFunc().get_dataBase_environment_config()
 mysql_info = dataBase_configure[0]
 mongo_info = dataBase_configure[1]
 
+# 获取基础路径配置
+url_configure = CommonFunc().get_BaseUrl_environment_config()    # 获取配置文件中后台的ip
+ip_address = url_configure[1]
+
 # 测试用例失败重跑,作用于类下面的所有用例
 # @pytest.mark.flaky(reruns=3, reruns_delay=10)
 @allure.feature('总台-代理报表-账目')
@@ -45,7 +49,7 @@ class Test_bill_yaml(object):
 
         with allure.step(f"执行测试用例:{title}"):
             Bf_log('bill').info(f"----------------开始执行:{title}------------------------")
-        url = url['mde_ip'] + url['url']
+        url = ip_address + url['url']
         with allure.step(f"请求地址 {url}"):
             Bf_log('bill').info(f'请求地址为:{url}')
 
@@ -99,7 +103,7 @@ class Test_bill_yaml(object):
 
         with allure.step(f"执行测试用例:{title}"):
             Bf_log('bill').info(f"----------------开始执行:{title}------------------------")
-        url = url['mde_ip'] + url['url']
+        url = ip_address + url['url']
         with allure.step(f"请求地址 {url}"):
             Bf_log('bill').info(f'请求地址为:{url}')
 
