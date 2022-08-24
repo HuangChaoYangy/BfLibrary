5d5af9ae99c568654332fae2a46e739924d88a43
liyang <liyang9096@gmail.com>
2022-08-24 19:12:46 +0700

    代理报表-佣金报表-新增获取基础url配置文件

--- credit_test_case/ReportManagement/test_commission_ya.py
+++ credit_test_case/ReportManagement/test_commission_ya.py
@@ -20,6 +20,10 @@ dataBase_configure = CommonFunc().get_dataBase_environment_config()
 mysql_info = dataBase_configure[0]
 mongo_info = dataBase_configure[1]
 
+# 获取基础路径配置
+url_configure = CommonFunc().get_BaseUrl_environment_config()    # 获取配置文件中后台的ip
+ip_address = url_configure[1]
+
 # 测试用例失败重跑,作用于类下面的所有用例
 # @pytest.mark.flaky(reruns=3, reruns_delay=10)
 @allure.feature('总台-报表管理-佣金报表')
@@ -43,7 +47,7 @@ class Test_dailyReport_yaml(object):
         actualResult = CreditBackGround(mysql_info,mongo_info).credit_rebateReport(inData=inBody,queryType=1)
         with allure.step(f"执行测试用例:{inBody['title']}"):
             Bf_log('commission').info(f"----------------开始执行:{inBody['title']}------------------------")
-        url = url['mde_ip'] + url['url']
+        url = ip_address + url['url']
         with allure.step(f"请求地址 {url}"):
             Bf_log('commission').info(f'请求地址为:{url}')
 
@@ -113,7 +117,7 @@ class Test_dailyReport_yaml(object):
 
         with allure.step(f"执行测试用例:{inBody['title']}"):
             Bf_log('commission').info(f"----------------开始执行:{inBody['title']}------------------------")
-        url = url['mde_ip'] + url['totalUrl']
+        url = ip_address + url['totalUrl']
         with allure.step(f"请求地址 {url}"):
             Bf_log('commission').info(f'请求地址为:{url}')
 
