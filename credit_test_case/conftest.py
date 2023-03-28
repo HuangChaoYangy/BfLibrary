0ad2b853f2b68865a278654c1d3129ff91e185b8
‘liyang’ <liyang9096@gmail.com>
2023-03-28 13:54:46 +0800

    pytest前后置,手动调用装饰器中的函数以及参数化

--- credit_test_case/conftest.py
+++ credit_test_case/conftest.py
@@ -22,18 +22,35 @@ def start_running():
 # 自动化测试止执行后 -- 数据清除操作
 
 
-
+# fixture固件
 @pytest.fixture(scope="session",autouse=False)
 def connetion_mysql():
     print(f'--- 马上开始执行自动化测试, 当前环境为：{environment} ---')
     yield   # 通过yield来唤醒teardown执行
     print('\n---自动化测试完成,开始处理本次测试数据---')
 
-
 # 在测试用例中手动调用 connetion_mysql 该方法
+
+@pytest.fixture(scope="session",autouse=False,params=["mysql","redis"])    # params 用法
+def connetion_mysql(request):        # request 固定参数
+    print(f'--- 马上开始执行自动化测试, 当前环境为：{environment} ---')
+    yield request.param  # 通过yield固定返回 request.param
+    print('\n---自动化测试完成,开始处理本次测试数据---')
 
 
 def test_case1(self, connetion_mysql):
+    url = ""
+    data = ""
+    return None
+
+
+@pytest.fixture(scope="session",autouse=False,name='cm')   # name 夹具别名
+def connetion_mysql():     # 夹具
+    print(f'--- 马上开始执行自动化测试, 当前环境为：{environment} ---')
+    yield   # 通过yield来唤醒teardown执行
+    print('\n---自动化测试完成,开始处理本次测试数据---')
+
+def test_case2(self, cm):   # name 夹具取了别名后，只能用别名不能用原来的名称
     url = ""
     data = ""
     return None
